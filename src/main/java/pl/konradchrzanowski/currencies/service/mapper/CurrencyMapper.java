31c6690cb422cd202cf6c390ed0fad68f4ed3d27
Konrad <chrzanekk@gmail.com>
2023-08-03 20:21:13 +0200

    implemented mapper

---
+++ src/main/java/pl/konradchrzanowski/currencies/service/mapper/CurrencyMapper.java
@@ -0,0 +1,18 @@
+package pl.konradchrzanowski.currencies.service.mapper;
+
+import org.mapstruct.Mapper;
+import pl.konradchrzanowski.currencies.domain.Currency;
+import pl.konradchrzanowski.currencies.service.dto.CurrencyDTO;
+
+@Mapper(componentModel = "spring", uses = {})
+public interface CurrencyMapper extends EntityMapper<CurrencyDTO, Currency> {
+
+    default Currency fromId(Long id) {
+        if(id == null) {
+            return null;
+        }
+        Currency currency = new Currency();
+        currency.setId(id);
+        return currency;
+    }
+}
